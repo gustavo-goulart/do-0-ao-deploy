f287b52757768a3f12934d27c23d1d691d8fb884
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-24 18:58:25 -0300

    tipos de dados

--- javascript/2-tipos-de-dados/script.js
+++ javascript/2-tipos-de-dados/script.js
@@ -5,3 +5,10 @@ let peso = 120; // Number -> interger
 let altura = 1.87; // Number -> float
 let temAnimalDeEstimacao = true; //Boolean
 let algo = null; //null
+
+//dados intercambiados - number -> string | string -> number
+// let telefone = "999999999";
+let telefone = "999999999";
+// telefone = telefone - 0;
+telefone = parseInt(telefone);
+console.log(typeof telefone);
