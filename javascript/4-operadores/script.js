be3d3b484f0f436390d12617c9053d880f7ec432
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-28 11:21:44 -0300

    operadores aritmeticos

---
+++ javascript/4-operadores/script.js
@@ -0,0 +1,15 @@
+let soma = 10 + 5;
+let subtracao = 10 - 5;
+let multiplicacao = 10 * 5;
+let divisao = 10 / 5;
+let restoDaDivisao = 10 % 5;
+
+let resultado1 = 10 - 5 * 15;
+let resultado2 = (10 - 5) * 15;
+let resultado3 = (10 / 5) * 15;
+let resultado4 = 10 / (5 * 15);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
