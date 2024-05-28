3abb14d53d674182f8478d38a7e7215f5ace8665
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-28 14:20:53 -0300

    exercicios

---
+++ javascript/0-exercicios/1-exercicio-variaveis/script.js
@@ -0,0 +1,7 @@
+const nome = "Gustavo";
+const idade = 42;
+const comidaFavorita = "Churrasco";
+
+console.log(
+  `Prazer me chamo ${nome}, tenho ${idade} de vida e adoro um belo ${comidaFavorita}`
+);
