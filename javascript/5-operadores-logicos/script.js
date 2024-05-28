929e37074ed4860313605dfb3521e2a78ce9eb0c
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-28 13:53:37 -0300

    operadores logicos

---
+++ javascript/5-operadores-logicos/script.js
@@ -0,0 +1,30 @@
+// TABELA VERDADE OU
+
+true || true; // true
+true || false; // true
+false || true; // true
+false || false; // false
+
+// TABELA VERDADE AND
+
+true && true; // true
+true && false; // false
+false && true; // false
+false && false; // false
+
+/* OPERADORES LÓGICOS
+> Maior que
+< Menor que
+>= Maior ou igual
+<= Menor ou igual
+== Igual a
+=== Igual e do mesmo tipo
+!= Diferente de 
+!== Diferente em valor e tipo
+*/
+
+texto = "meu tamanho de texto";
+console.log(texto.length);
+
+resultado = texto.length >= 40;
+console.log(resultado);
